20b0f30a7bf986855fd2425201fd7bb02a7f396b
Gabe Koeb <gfkoeb@ncsu.edu>
2024-03-10 00:35:08 -0500

    Add nvim setup

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,14 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local config = require("nvim-treesitter.configs")
+    config.setup({
+      ensure_installed = { "c", "typescript", "java", "css", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end
+
+}
+
