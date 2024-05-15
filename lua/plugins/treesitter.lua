37480cee53b418f5a5d86ac6bb702f7a8bc2ee4b
Gabe Koeb <gfkoeb@ncsu.edu>
2024-05-15 12:10:34 -0400

    Adding autotag

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,26 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    local config = require("nvim-treesitter.configs")
-    config.setup({
-      ensure_installed = { "c", "typescript", "java", "css", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
-  end
-
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local config = require("nvim-treesitter.configs")
+		config.setup({
+			ensure_installed = {
+				"c",
+				"typescript",
+				"java",
+				"css",
+				"lua",
+				"vim",
+				"vimdoc",
+				"query",
+				"elixir",
+				"heex",
+				"javascript",
+				"html",
+			},
+			highlight = { enable = true },
+			indent = { enable = true },
+			autotag = { enable = true },
+		})
+	end,
 }
-
