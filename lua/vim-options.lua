f0842c35231994dc09c003193518ef67855d0853
Gabe Koeb <gfkoeb@ncsu.edu>
2024-07-18 20:12:18 -0400

    update config

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -10,7 +10,7 @@ vim.cmd("set relativenumber")
 vim.cmd("set guicursor=i:block")
 vim.cmd("lua vim.diagnostic.open_float()")
 
---primagen stuff--
+--primeagen stuff--
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
