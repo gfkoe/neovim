184cea362efac240c890a91b55be4288d9a1ebc8
Gabe Koeb <gfkoeb@ncsu.edu>
2024-09-20 15:39:30 -0400

    update nvim

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,7 +1,7 @@
 return {
 	"ThePrimeagen/harpoon",
 	branch = "harpoon2",
-	depedencies = { "nvim-lua/plenary.nvim" },
+	depedencies = { "nvim-lua/plenary.nvim", "nvim-lua/telescope.nvim" },
 	config = function()
 		local harpoon = require("harpoon")
 		harpoon:setup()
@@ -19,18 +19,19 @@ return {
 		vim.keymap.set("n", "<C-t>", function()
 			harpoon:list():select(2)
 		end)
-		vim.keymap.set("n", "<C-n>", function()
+		--[[vim.keymap.set("n", "<C-n>", function()
 			harpoon:list():select(3)
 		end)
 		vim.keymap.set("n", "<C-s>", function()
 			harpoon:list():select(4)
-		end)
+		end)]]
+		--
 
 		-- Toggle previous & next buffers stored within Harpoon list
-		vim.keymap.set("n", "<C-S-P>", function()
+		vim.keymap.set("n", "<C-m>", function()
 			harpoon:list():prev()
 		end)
-		vim.keymap.set("n", "<C-S-N>", function()
+		vim.keymap.set("n", "<C-n>", function()
 			harpoon:list():next()
 		end)
 		local conf = require("telescope.config").values
