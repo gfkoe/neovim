9d4edc886669b81775aef94a2ccc2687866c0725
Gabe Koeb <gfkoeb@ncsu.edu>
2024-10-10 13:52:20 -0400

    update conform

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -9,18 +9,18 @@ return {
 
 		conform.setup({
 			formatters_by_ft = {
-				lua = { { "stylua" } },
-				javascript = { { "prettierd", "prettier" } },
-				typescript = { { "prettierd", "prettier" } },
-				javascriptreact = { { "prettierd", "prettier" } },
-				typescriptreact = { { "prettierd", "prettier" } },
-				css = { { "prettierd", "prettier" } },
-				html = { { "prettierd", "prettier" } },
-				json = { { "prettierd", "prettier" } },
-				markdown = { { "prettierd", "prettier" } },
+				lua = { "stylua" },
+				javascript = { "prettierd", "prettier", stop_after_first = true },
+				typescript = { "prettierd", "prettier", stop_after_first = true },
+				javascriptreact = { "prettierd", "prettier" },
+				typescriptreact = { "prettierd", "prettier" },
+				css = { "prettierd", "prettier" },
+				html = { "prettierd", "prettier" },
+				json = { "prettierd", "prettier" },
+				markdown = { "prettierd", "prettier" },
 			},
 			format_on_save = {
-				lsp_fallback = true,
+				lsp_fallback = "fallback",
 				async = false,
 				timeout_ms = 500,
 			},
