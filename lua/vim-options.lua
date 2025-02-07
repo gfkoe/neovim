b154689282cb1a8401f4ccb1ad30366ae38db09a
Gabe Koeb <gfkoeb@ncsu.edu>
2025-02-07 18:31:46 -0500

    update tab

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,9 +1,9 @@
 vim.opt.guicursor = ""
 
 vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
 vim.cmd("set number")
 vim.cmd("set smartindent")
 
