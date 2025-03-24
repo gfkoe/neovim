8cb1d093b533803401b4801039c22b56fb14430f
Gabe Koeb <gfkoeb@ncsu.edu>
2025-03-24 19:27:39 -0400

    update typo

---
+++ lua/plugins/dev.lua
@@ -0,0 +1,3 @@
+return {
+	{ dir = "~/PersonalProjects/ezprev.nvim" },
+}
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -28,8 +28,8 @@ return {
 				use_nvim_cmp_as_default = true,
 				nerd_font_variant = "mono",
 			},
+			signature = { enabled = true },
 		},
-		signatue = { enabled = true },
 	},
 	--{
 	--	"hrsh7th/nvim-cmp",
